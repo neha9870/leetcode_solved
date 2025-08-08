7ef224f900d78095cdd2d6db0332cf6c7ead7c49
neha9870 <neha.dev.9870@gmail.com>
2025-08-08 15:59:09 +0530

    Add Numbers

--- problem2.py
+++ problem2.py
@@ -7,10 +7,7 @@
 
 # You may assume the two numbers do not contain any leading zero, except the number 0 itself.
 
- 
-
 # Example 1:
-
 
 # Input: l1 = [2,4,3], l2 = [5,6,4]
 # Output: [7,0,8]
@@ -24,7 +21,6 @@
 # Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
 # Output: [8,9,9,9,0,0,0,1]
  
-
 # Constraints:
 
 # The number of nodes in each linked list is in the range [1, 100].
@@ -56,7 +52,6 @@ sum1 = list(str(sum))
 
 sum1.reverse()
 new = list(map(int,sum1))
-
 
 # print(sum)
 print(sum1)
